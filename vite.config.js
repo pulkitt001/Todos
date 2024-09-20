f98ad421819b98de56144bba2305e72fc479a618
pulkitt001 <pulkitpal0001@gmail.com>
2024-09-20 13:24:05 +0530

    add gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Todos/', // The base path corresponds to your GitHub Pages repository name
+  base: "/Todos/", // The base path corresponds to your GitHub Pages repository name
 })
