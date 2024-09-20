3bffa4ecf67ccdc26c4200c9ff2287cefed22bea
pulkitt001 <pulkitpal0001@gmail.com>
2024-09-20 12:48:39 +0530

    add path

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Todos/', // The base path corresponds to your GitHub Pages repository name
 })
